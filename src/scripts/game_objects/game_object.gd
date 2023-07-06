954f713b2f145fa8e62933b5e436cb0f5153be00
ryanabx <ryanbrue@hotmail.com>
2023-07-05 20:24:17 -0500

    Progress

--- src/scripts/game_objects/game_object.gd
+++ src/scripts/game_objects/game_object.gd
@@ -7,9 +7,14 @@ var mouse_offset_vect: Vector2 = Vector2.ZERO
 
 func _input(event):
 	if event is InputEventMouseButton:
-		if get_rect().has_point(to_local(event.position)):
+		if get_rect().has_point(to_local(event.position) - get_viewport_transform().origin):
 			grabbable = event.pressed
-			mouse_offset_vect = position - get_global_mouse_position()
+			mouse_offset_vect = global_position - get_global_mouse_position()
+	if event is InputEventMouseMotion:
+		if get_rect().has_point(to_local(event.global_position) - get_viewport_transform().origin):
+			modulate.a = 0.7
+		else:
+			modulate.a = 1.0
 
 func _process(delta):
 	if grabbable and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
