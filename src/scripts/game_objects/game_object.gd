46474fe405a4a7d521ebd5b43e8be48d7bd9a7cb
ryanabx <ryanbrue@hotmail.com>
2023-07-05 19:43:21 -0500

    Implement grabbable game objects

--- src/scripts/game_objects/game_object.gd
+++ src/scripts/game_objects/game_object.gd
@@ -1,1 +1,16 @@
-extends Node2D
+extends Sprite2D
+
+var grabbable: bool = false
+var mouse_offset_vect: Vector2 = Vector2.ZERO
+
+@onready var sprite = $Sprite
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if get_rect().has_point(to_local(event.position)):
+			grabbable = event.pressed
+			mouse_offset_vect = position - get_global_mouse_position()
+
+func _process(delta):
+	if grabbable and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		position = get_global_mouse_position() + mouse_offset_vect
