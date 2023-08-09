adb4889b71c8db8bb9cf7ccbf1a924ac9f1a0a8f
Ryan Brue <ryanbrue@hotmail.com>
2023-08-08 20:38:12 -0500

    Serialize and deserialize

--- src/scripts/game_elements/gobjects/collection.gd
+++ src/scripts/game_elements/gobjects/collection.gd
@@ -2,7 +2,7 @@ class_name Collection
 extends Gobject
 
 @export var base_size: Vector2 = Vector2.ONE
-@export var inside: Array[String] = []
+@export var inside: Array = []
 @export var view_perms: Array = []
 @export var access_perms: Array = []
 @export var permanent: bool = false
@@ -11,4 +11,15 @@ extends Gobject
 
 enum Type {STACK, HAND}
 
+func serialize() -> Dictionary:
+    var result: Dictionary = {}
+    for prop in get_property_list():
+        var _n: StringName = prop.name
+        result[_n] = get(_n)
+    return result
 
+static func deserialize(_d: Dictionary) -> Collection:
+    var piece: Collection = Collection.new()
+    for prop in _d.keys():
+        piece.set(prop, _d[prop])
+    return piece
--- src/scripts/game_elements/gobjects/piece.gd
+++ src/scripts/game_elements/gobjects/piece.gd
@@ -5,3 +5,17 @@ extends Gobject
 @export var image_down: String = ""
 @export var collection: String = ""
 @export var face_up: bool = false
+
+
+func serialize() -> Dictionary:
+    var result: Dictionary = {}
+    for prop in get_property_list():
+        var _n: StringName = prop.name
+        result[_n] = get(_n)
+    return result
+
+static func deserialize(_d: Dictionary) -> Piece:
+    var piece: Piece = Piece.new()
+    for prop in _d.keys():
+        piece.set(prop, _d[prop])
+    return piece
