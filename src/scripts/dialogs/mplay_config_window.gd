a7edf9787b8270509380aa0a3aed2046348c9c7d
Ryan Brue <ryanbrue@hotmail.com>
2023-08-16 10:11:36 -0500

    Create gui for multiplayer connection

--- src/scripts/dialogs/mplay_config_window.gd
+++ src/scripts/dialogs/mplay_config_window.gd
@@ -1,29 +1,12 @@
 extends Window
 
+
 func _ready() -> void:
-	SignalManager.open_multiplayer_menu.connect(open_window)
-	if OS.get_name() == "Web":
-		$Container/VBoxContainer/HBoxContainer2.hide()
-		$Container/VBoxContainer/WebNotice.show()
+	%CreateServer.pressed.connect(_create_server_button)
+	%CreateClient.pressed.connect(_create_client_button)
 
-func open_window() -> void:
-	print("opening window")
-	popup()
+func _create_server_button():
+	pass
 
-func _on_create_server_pressed() -> void:
-	var port: String = $Container/VBoxContainer/HBoxContainer2/Port.text
-	if MultiplayerManager.create_server(int(port)):
-		hide()
-		get_tree().reload_current_scene()
-
-func _on_create_client_pressed() -> void:
-	var port: String = $Container/VBoxContainer/HBoxContainer/Port.text
-	var ip: String = $Container/VBoxContainer/HBoxContainer/IPAddress.text
-
-	if MultiplayerManager.create_client(int(port), ip):
-		hide()
-		get_tree().reload_current_scene()
-
-
-func _on_close_requested() -> void:
-	hide()
+func _create_client_button():
+	pass
