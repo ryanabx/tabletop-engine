a2442ec12b001c46941f1bbd9bf395f4d76b91fb
ryanabx <ryanbrue@hotmail.com>
2023-08-26 15:46:47 -0500

    More things

--- src/scripts/menu/main_menu.gd
+++ src/scripts/menu/main_menu.gd
@@ -20,4 +20,4 @@ func _on_quit_pressed() -> void:
 
 
 func _on_settings_pressed() -> void:
-	pass # Replace with function body.
+	SignalManager.scene_transition.emit("res://src/scenes/menu/settings.tscn")
