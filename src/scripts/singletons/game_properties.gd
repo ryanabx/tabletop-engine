504657a0c054bc8a02f1034d1737131d033500e4
ryanabx <ryanbrue@hotmail.com>
2023-09-05 00:32:32 -0500

    Settings fixed

--- src/scripts/singletons/game_properties.gd
+++ src/scripts/singletons/game_properties.gd
@@ -6,6 +6,11 @@ enum SelectType {
 enum MultiplayerMethod {
 	P2P_SHORTENED, P2P
 }
+
+var properties: Array[String] = [
+	"fullscreen", "select_type", "multiplayer_method"
+]
+
 # View settings
 var fullscreen: bool:
 	set(val):
@@ -13,6 +18,7 @@ var fullscreen: bool:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 		else:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		return true
 	get:
 		return (DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN)
 
@@ -23,21 +29,21 @@ var select_type: SelectType = SelectType.DRAG
 var multiplayer_method: MultiplayerMethod = MultiplayerMethod.P2P_SHORTENED
 
 func _ready() -> void:
+	print("HI")
 	load_settings()
 
 func load_settings() -> void:
-	print(get_property_list())
 	if FileAccess.file_exists(Globals.SETTINGS_PATH):
 		var settings_str: String = FileAccess.get_file_as_string(Globals.SETTINGS_PATH)
 		var settings_dict: Dictionary = JSON.parse_string(settings_str)
 		for prop in settings_dict.keys():
 			set(prop, settings_dict[prop])
-		print("Settings loaded!")
+	print("Settings loaded!")
 
 func save_settings() -> void:
 	var settings_dict: Dictionary = {}
-	for prop in get_property_list():
-		settings_dict[prop.name] = get(prop.name)
+	for prop in properties:
+		settings_dict[prop] = get(prop)
 	var settings_str: String = JSON.stringify(settings_dict)
 	var s_save: FileAccess = FileAccess.open(Globals.SETTINGS_PATH, FileAccess.WRITE)
 	s_save.store_string(settings_str)
