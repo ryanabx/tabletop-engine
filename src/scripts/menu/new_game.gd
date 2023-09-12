5df187d1078cf2509086e88cc69ba511524a1b0f
ryanabx <ryanbrue@hotmail.com>
2023-09-11 20:58:00 -0500

    wip multiplayer stuff

--- src/scripts/menu/new_game.gd
+++ src/scripts/menu/new_game.gd
@@ -4,36 +4,17 @@ extends Control
 @onready var ready_button: Button = %ReadyButton
 @onready var delete_button: Button = %DeleteSelected
 @onready var from_file_button: Button = %LoadConfFile
-@onready var create_server_button: Button = %CreateServerButton
-@onready var connect_client_button: Button = %ConnectClientButton
-@onready var server_list: ItemList = %ServerList
-
-@onready var client_code_entry: LineEdit = %ClientCodeEntry
-@onready var server_cancel_button: Button = %ServerCancelButton
-@onready var refresh_server_list_button: Button = %RefreshServerListings
-
-var peer_choice: int = 0 # Client
-
-var current_mplay_page: String = "Options1"
-
-var code: String = ""
 
 func _ready() -> void:
 	Globals.load_this_game = PackedByteArray([])
 	SignalManager.config_added.connect(refresh_list)
 	if not Utils.is_desktop_platform():
 		from_file_button.hide()
-	MultiplayerManager.close_connection()
-	SignalManager.mplay_code_created.connect(code_created)
 	refresh_list()
 
 func _process(_delta: float) -> void:
 	ready_button.disabled = not is_selecting_config() or not multiplayer.is_server()
 	delete_button.disabled = not is_selecting_config() or is_selecting_default_config()
-	connect_client_button.disabled = not is_selecting_server()
-	
-	refresh_server_list_button.visible = not (multiplayer.multiplayer_peer is WebRTCMultiplayerPeer)
-	server_cancel_button.visible = (multiplayer.multiplayer_peer is WebRTCMultiplayerPeer)
 
 
 
@@ -47,13 +28,6 @@ func refresh_list() -> void:
 	for conf: String in Utils.FileManager.get_available_configs():
 		config_list.add_item(conf)
 	print("Config list refreshed!")
-
-func refresh_server_list() -> void:
-	server_list.clear()
-	var servers_list: Array = await MultiplayerManager.get_server_list()
-	print(servers_list)
-	for x: String in servers_list:
-		server_list.add_item(x)
 
 
 func _on_ready_pressed() -> void:
@@ -79,12 +53,6 @@ func is_selecting_config() -> bool:
 func is_selecting_default_config() -> bool:
 	return is_selecting_config() and config_list.get_selected_items()[0] == 0
 
-func is_selecting_server() -> bool:
-	return not server_list.get_selected_items().is_empty()
-
-func get_selected_server() -> String:
-	return config_list.get_item_text(server_list.get_selected_items()[0])
-
 func get_config_file_path(conf_name: String) -> String:
 	return str(Globals.CONFIG_REPO,"/",conf_name,Globals.CONFIG_EXTENSION)
 
@@ -97,34 +65,3 @@ func _on_delete_selected_pressed() -> void:
 
 func _on_load_conf_url_pressed() -> void:
 	SignalManager.download_config_popup.emit()
-
-# MULTIPLAYER
-
-func code_created(_code: String) -> void:
-	OS.alert(str("Code created: ",_code), "Remember this code")
-
-
-func _on_refresh_server_listings_pressed() -> void:
-	refresh_server_list()
-
-
-func _on_create_server_button_pressed() -> void:
-	MultiplayerManager.create_server()
-	create_server_button.visible = false
-	client_code_entry.visible = true
-
-
-func _on_connect_client_button_pressed() -> void:
-	MultiplayerManager.create_client(get_selected_server())
-
-
-func _on_server_cancel_button_pressed() -> void:
-	MultiplayerManager.close_connection()
-	create_server_button.visible = true
-	client_code_entry.visible = false
-	
-
-
-func _on_client_code_entry_text_submitted(new_text: String) -> void:
-	SignalManager.client_code_received.emit(new_text)
-
