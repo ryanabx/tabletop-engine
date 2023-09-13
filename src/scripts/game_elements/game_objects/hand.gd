c7a45ad3ea6ef40f1977ec7bebfd72e9c3be48cc
ryanabx <ryanbrue@hotmail.com>
2023-09-13 18:51:37 -0500

    Update hand.gd

--- src/scripts/game_elements/game_objects/hand.gd
+++ src/scripts/game_elements/game_objects/hand.gd
@@ -1,3 +1,17 @@
 class_name Hand
 extends Collection
 
+enum VisibilitySetting {
+    ALL_PLAYERS,
+    DESIGNATED_PLAYERS,
+    NOT_DESIGNATED_PLAYERS,
+    NO_PLAYERS
+}
+
+# Shareable properties
+var visibility: VisibilitySetting = VisibilitySetting.DESIGNATED_PLAYERS
+var designated_players: Array[int] = []
+
+func get_shareable_properties() -> Array:
+    return super.get_shareable_properties() + ["visibility", "designated_players"]
+
