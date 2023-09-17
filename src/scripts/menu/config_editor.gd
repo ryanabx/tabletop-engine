2e2fca5ab67479d03b0c5184a11e6409a48d105b
Ryan Brue <ryanbrue@hotmail.com>
2023-09-16 19:19:13 -0500

    Add basic builtin code editor

---
+++ src/scripts/menu/config_editor.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+    pass
+
+
+func _on_back_button_pressed():
+    SignalManager.scene_transition.emit("res://src/scenes/menu/main_menu.tscn")
--- src/scripts/menu/main_menu.gd
+++ src/scripts/menu/main_menu.gd
@@ -22,3 +22,7 @@ func _on_quit_pressed() -> void:
 
 func _on_settings_pressed() -> void:
     SignalManager.scene_transition.emit("res://src/scenes/menu/settings.tscn")
+
+
+func _on_configurations_pressed():
+    SignalManager.scene_transition.emit("res://src/scenes/menu/config_editor.tscn")
