6e06349820139a9a55936c78cd2bfe57cfc5310f
ryanabx <ryanbrue@hotmail.com>
2023-09-20 16:19:47 -0500

    Desktop vs mobile buttons

--- src/scripts/menu/main_menu.gd
+++ src/scripts/menu/main_menu.gd
@@ -1,18 +1,15 @@
 extends Control
 
 @onready var version := %Version
-@onready var quit_button := %Quit
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
     version.text = str("Version: ",ProjectSettings.get_setting("application/config/version"))
-    if Utils.is_mobile_platform():
-        quit_button.hide()
-    Globals.load_this_game = PackedByteArray([])
+    if not Utils.is_desktop_platform():
+        %DesktopButtons.hide()
+        %MobileButtons.show()
 
-func _process(_delta: float) -> void:
-    # %ButtonFlow.vertical = get_viewport_rect().size.y > get_viewport_rect().size.x
-    pass
+    Globals.load_this_game = PackedByteArray([])
 
 func _on_new_game_pressed() -> void:
     SignalManager.scene_transition.emit("res://src/scenes/menu/new_game.tscn")
@@ -26,5 +23,9 @@ func _on_settings_pressed() -> void:
     SignalManager.scene_transition.emit("res://src/scenes/menu/settings.tscn")
 
 
-func _on_configurations_pressed():
+func _on_configurations_pressed() -> void:
     SignalManager.scene_transition.emit("res://src/scenes/menu/config_editor.tscn")
+
+
+func _on_about_pressed() -> void:
+    pass # Replace with function body.
