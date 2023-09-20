57b8a9a7d1b8b3f9ec84d968de724ad7628b942a
ryanabx <ryanbrue@hotmail.com>
2023-09-20 13:26:59 -0500

    AAAAA

--- src/scripts/dialogs/load_config_dialog.gd
+++ src/scripts/dialogs/load_config_dialog.gd
@@ -12,13 +12,16 @@ func filepath_selected(fname: String) -> void:
     print(fname)
     if FileAccess.file_exists(fname):
         file_decided(FileAccess.get_file_as_bytes(fname))
+    else:
+        print("File at ",fname, " doesn't exist!")
     
 func file_decided(buf: PackedByteArray) -> void:
+    print("Importing config")
     var config: TabletopGame = TabletopGame.import_config(buf)
     
     Utils.FileManager.create_dir(Globals.CONFIG_REPO)
 
-    var conf_path: String = str(Globals.CONFIG_REPO, "/",config.export_settings().name,Globals.CONFIG_EXTENSION)
+    var conf_path: String = str(Globals.CONFIG_REPO, "/",config.name,Globals.CONFIG_EXTENSION)
 
     Utils.FileManager.delete_file(conf_path)
     
@@ -30,5 +33,6 @@ func file_decided(buf: PackedByteArray) -> void:
 
     local_copy.store_buffer(buf)
     local_copy.close()
+    print("Done! Sent to ",conf_path)
     SignalManager.config_added.emit()
 
