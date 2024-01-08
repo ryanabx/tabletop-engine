04ab8cbb29a4ab4b53ec131de0a904af80edb5b5
Ryan Brue <ryanbrue@hotmail.com>
2024-01-08 17:05:38 -0600

    More GmCard implementation

--- tabletop-engine/src_cs/tabletop/objects/standard/GmCard.cs
+++ tabletop-engine/src_cs/tabletop/objects/standard/GmCard.cs
@@ -4,5 +4,14 @@ namespace TabletopEngine.Tabletop.Objects.Standard;
 
 public partial class GmCard : GmPiece, IFlippable
 {
-	
+	private bool _orientation;
+	public bool Orientation
+	{
+		get { return _orientation; }
+		set { _orientation = value; }
+	}
+	public void Flip()
+	{
+		Orientation = !Orientation;
+	}
 }
